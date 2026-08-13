f4eac60e9c55df6c10ff163ea00fbc4bf90df627
Girish Laathar <82574048+giprish@users.noreply.github.com>
2026-08-13 14:47:00 +0530

    add the link of the number

--- app/service-areas/[county]/[city]/CityPageClient.tsx
+++ app/service-areas/[county]/[city]/CityPageClient.tsx
@@ -432,15 +432,15 @@ export default function GenericCityDetailPage({ cityData }: { cityData: CityData
                   >
                     Get Expert Mortgage Advice
                   </Link>
-                  <Link prefetch={false}
-                    href="/contact-us/"
+                  <a
+                    href="tel:+16025352171"
                     className="border border-white text-white font-bold text-[14.5px] px-6 py-3.5 rounded-lg hover:bg-white/10 transition-all duration-300 flex items-center gap-2"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                       <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                     </svg>
                     Call Us Now
-                  </Link>
+                  </a>
                 </div>
               </div>
             </div>
